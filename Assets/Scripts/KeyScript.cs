39788d09845b1cb63ba4a92cdf141984cf680510
SAE\stcollins <tiger.collins@outlook.com>
2017-09-20 21:06:22 +1000

    First Commit

---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyScript : MonoBehaviour {
+
+
+    public void Awake()
+    {
+
+    }
+
+    public void KeyClicked()
+    {
+        KeyboardInputField.Instance.UpdateInputField(gameObject.name);
+    }
+
+    public void ContinueClicked()
+    {
+        Debug.Log("CLICKED");
+        KeyboardInputField.Instance.NameSelection += 1;
+        KeyboardInputField.Instance.FirstNameField();
+    }
+
+}
---
+++ Assets/Scripts/KeyboardInputField.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class KeyboardInputField : MonoBehaviour {
+
+    public static KeyboardInputField Instance;
+
+    public Text FirstName;
+    public Text MiddleName;
+    public Text LastName;
+
+    public Text InputField;
+
+    public int NameSelection = 0;
+
+
+
+    // Use this for initialization
+    void Awake ()
+    {
+        Instance = this;
+       // FirstName.text = "blah";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            
+        }
+    }
+
+    public void UpdateInputField(string input)
+    {
+        if (input == "Space")
+        {
+            InputField.text += " ";
+        }
+
+        else
+        {
+            InputField.text += input;
+        }
+    }
+
+    public void FirstNameField()
+    {
+        Debug.Log("name select" + NameSelection + " text is: " + InputField.text);
+        if (NameSelection == 1)
+        {
+            FirstName.text = InputField.text;
+
+        }
+
+        if (NameSelection == 2)
+        {
+            MiddleName.text = InputField.text;
+        }
+
+        if (NameSelection == 3)
+        {
+        }
+
+        if (NameSelection == 4)
+        {
+        }
+
+    }
+
+
+}
